b31a556f7203131a150de4ecf13888294b0607cc
MaxKostin03 <94163240+MaxKostin03@users.noreply.github.com>
2023-02-02 16:09:06 +0300

    Add files via upload

---
+++ DZ5/QtLanguage_en_US.ts
@@ -0,0 +1,139 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="form.ui" line="14"/>
+        <source>Справка</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="form.ui" line="20"/>
+        <source>TextLabel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Открыть&lt;/p&gt;&lt;p&gt;Сохранить&lt;/p&gt;&lt;p&gt;Справка&lt;/p&gt;&lt;p&gt;Выход&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="vanished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Open&lt;/p&gt;&lt;p&gt;Save&lt;/p&gt;&lt;p&gt;Help&lt;/p&gt;&lt;p&gt;Exit&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>По умолчанию</source>
+        <translation type="vanished">Default</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Блокнот</source>
+        <translation>Notepadjust++</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="54"/>
+        <source>Файл</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="61"/>
+        <source>Язык</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="90"/>
+        <source>Сохранить</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="93"/>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="98"/>
+        <source>Открыть</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="101"/>
+        <source>Ctrl+O</source>
+        <translation>Ctrl+O</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="106"/>
+        <source>Выход</source>
+        <translation>Exit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="109"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="114"/>
+        <source>Справка</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="138"/>
+        <source>Открыть для чтения</source>
+        <oldsource>Открыть в режиме чтения</oldsource>
+        <translation>Оpen read-only</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="149"/>
+        <source>Светлая</source>
+        <translation>Light</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="157"/>
+        <source>Тёмная</source>
+        <translation>Dark</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="68"/>
+        <source>Выбрать тему</source>
+        <translation>Theme</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="125"/>
+        <source>Русский</source>
+        <translation>Russian</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="133"/>
+        <source>Английский</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="45"/>
+        <location filename="mainwindow.cpp" line="65"/>
+        <source>Открыть файл</source>
+        <translation>Open file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="46"/>
+        <location filename="mainwindow.cpp" line="66"/>
+        <location filename="mainwindow.cpp" line="86"/>
+        <source>Текстовые файлы (*.txt);;Файлы C++ (*.cpp *.h)</source>
+        <translation>text file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="52"/>
+        <location filename="mainwindow.cpp" line="72"/>
+        <source>Ошибка</source>
+        <translation>error</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="52"/>
+        <location filename="mainwindow.cpp" line="72"/>
+        <source>Невозможно открыть файл</source>
+        <translation>Unable to open file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="85"/>
+        <source>Сохранить файл</source>
+        <translation>save file</translation>
+    </message>
+</context>
+</TS>
---
+++ DZ5/mainwindow.h
@@ -0,0 +1,48 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtWidgets>
+#include <QKeyEvent>
+#include <QEvent>
+#include <QSharedPointer>
+
+#include "form.h"
+#include "filemanager.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+protected:
+    virtual void changeEvent(QEvent* event) override;
+
+private slots:
+    void open();
+    void openOnlyRead();
+    void save();
+    void help();
+    void changeLanguageUS();
+    void changeLanguageRU();
+    void on_dark_clicked();
+    void on_light_clicked();
+    void changePath(QFileInfo fileInfo);
+
+private:
+    Ui::MainWindow *ui;
+    QTranslator translator;
+    fileManager* f;
+    QShortcut* openKey;
+    QShortcut* saveKey;
+    QShortcut* helpKey;
+    QShortcut* quitKey;
+};
+#endif // MAINWINDOW_H
